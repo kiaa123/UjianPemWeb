2b240ea2bff9f3e7a8934c9efe62cedfe3436acb
Kiaaaaaaaaaa <sefryanso123@gmail.com>
2022-11-28 15:16:50 +0700

    first commit

---
+++ hizki12rpl/no7.php
@@ -0,0 +1,30 @@
+?php
+
+class Customer {
+public $id, $nama, $alamat, $negara;
+}
+$customer1 = new Customer();
+$customer2 = new Customer();
+$customer3 = new Customer();
+$customer4 = new Customer();
+
+echo 'ID: ' . $customer1->id = 1 . '<br>';
+echo 'Nama: ' . $customer1->nama = "mar" . '<br>';
+echo 'Alamat: ' . $customer1->alamat = "penuin" . '<br>';
+echo 'Negara: ' . $customer1->negara = "indonesia" . '<hr>';
+
+echo 'ID: ' . $customer2->id = 2 . '<br>';
+echo 'Nama: ' . $customer2->nama = "Anai" . '<br>';
+echo 'Alamat: ' . $customer2->alamat = "Mexico" . '<br>';
+echo 'Negara: ' . $customer2->negara = "Mexico" . '<hr>';
+
+echo 'ID: ' . $customer3->id = 3 . '<br>';
+echo 'Nama: ' . $customer3->nama = "Andi" . '<br>';
+echo 'Alamat: ' . $customer3->alamat = "US" . '<br>';
+echo 'Negara: ' . $customer3->negara = "Mexico" . '<hr>';
+
+echo 'ID: ' . $customer4->id = 4 . '<br>';
+echo 'Nama: ' . $customer4->nama = "Thomas" . '<br>';
+echo 'Alamat: ' . $customer4->alamat = "London" . '<br>';
+Foto profil familia_pakpahan
+ech
